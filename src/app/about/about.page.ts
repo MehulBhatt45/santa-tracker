ecd715572dce0df940aacd2aac55052a262a083b
mehulbhatt45 <mehul.2287884@gmail.com>
2020-02-19 19:14:27 +0530

    call us plugin

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵɵresolveBody } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+import loadjs from 'loadjs';
+declare const $: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
@@ -8,13 +9,24 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 })
 export class AboutPage implements OnInit {
   websiteUrl = 'https://santatracker7.wixsite.com/santatracker';
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser) {
+  }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+    // tslint:disable-next-line: max-line-length
+    // document.getElementById('MightyCallCBW-658960de-a625-4ae2-9957-e7b425e2b350').style.setProperty('display', 'inline-block', 'important');
+  }
+
   openWebsite() {
     const browser = this.iab.create(this.websiteUrl, '_system');
+  }
+
+  ionViewWillLeave() {
+    // document.getElementById('MightyCallCBW-658960de-a625-4ae2-9957-e7b425e2b350').style.setProperty('display', 'none', 'important');
+    // document.getElementById('mc__modal_658960de-a625-4ae2-9957-e7b425e2b350').style.setProperty('display', 'none', 'important');
   }
 
 }
